ecc6d3f0e3a37da58642fbf597d92acd2dd9dd41
Thielson <thielson12@gmail.com>
2022-07-21 22:10:31 -0300

    Correção no tamanho das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios";
 import { Dashboard } from "./pages/Dashboard";
 import { TablePage } from "./pages/TablePage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard/>}/>
-          <Route path="/tabela" element={<TablePage/>}/>
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/tabela" element={<TablePage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
--- src/components/Tabela/index.tsx
+++ src/components/Tabela/index.tsx
@@ -1,25 +1,28 @@
 import classNames from "classnames";
 import styles from "./styles.module.scss";
 
-export function Table(){
-    return(
-        <div className={styles.conteiner}>
-            <header className={styles.headerConteiner}>
-                <div className={classNames(styles.col,styles.col0)}> ID OS              </div>
-                <div className={classNames(styles.col,styles.col1)}> Operador           </div>
-                <div className={classNames(styles.col,styles.col2)}> Zona               </div>
-                <div className={classNames(styles.col,styles.col3)}> ID Cliente         </div>
-                <div className={classNames(styles.col,styles.col4)}> Tipo OS            </div>
-                <div className={classNames(styles.col,styles.col5)}> Equipe             </div>
-                <div className={classNames(styles.col,styles.col6)}> Transporte         </div>
-                <div className={classNames(styles.col,styles.col7)}> Data Abertura      </div>
-                <div className={classNames(styles.col,styles.col8)}> Data Finalização   </div>
-                <div className={classNames(styles.col,styles.col9)}>TRS (hrs)          </div>
-                <div className={classNames(styles.col,styles.col10)}>Taxa               </div>
-                <div className={classNames(styles.col,styles.col11)}>Satus Correção     </div>
-                <div className={classNames(styles.col,styles.col12)}>Pontos             </div>
-            </header>
-            Table
+export function Table() {
+  return (
+    <div className={styles.conteiner}>
+      <header className={styles.headerConteiner}>
+        <div className={classNames(styles.col, styles.col0)}>ID OS</div>
+        <div className={classNames(styles.col, styles.col1)}>Operador</div>
+        <div className={classNames(styles.col, styles.col2)}>Zona</div>
+        <div className={classNames(styles.col, styles.col3)}>ID Cliente</div>
+        <div className={classNames(styles.col, styles.col4)}>Tipo OS</div>
+        <div className={classNames(styles.col, styles.col5)}>Equipe</div>
+        <div className={classNames(styles.col, styles.col6)}>Transporte</div>
+        <div className={classNames(styles.col, styles.col7)}>Data Abertura</div>
+        <div className={classNames(styles.col, styles.col8)}>
+          Data Finalização
         </div>
-    )
+        <div className={classNames(styles.col, styles.col9)}>TRS (hrs)</div>
+        <div className={classNames(styles.col, styles.col10)}>Taxa</div>
+        <div className={classNames(styles.col, styles.col11)}>
+          Satus Correção
+        </div>
+        <div className={classNames(styles.col, styles.col12)}>Pontos</div>
+      </header>
+    </div>
+  );
 }
