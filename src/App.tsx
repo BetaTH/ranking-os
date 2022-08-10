424292ab0ac690e157ede85cc44e2527faae40d1
Thielson Almendra <thielson12@gmail.com>
2022-08-10 17:31:18 -0300

    firt version 1.5

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { TablePage } from "./pages/TablePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard } from "./pages/Dashboard";
 import "./styles/index.scss";
-import StateContextComponent from "./Teste/Context/StateContextComponent";
 import { AdminPage } from "./pages/AdminPage";
 import { LoginPage } from "./pages/LoginPage";
 import { SocketContextProvider } from "./contexts/SocketContext/SocketContext";
@@ -12,50 +11,48 @@ import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <StateContextComponent>
-      <SocketContextProvider>
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <LoginPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <DashPageContextProvider>
-                    <Dashboard />
-                  </DashPageContextProvider>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/tabela"
-              element={
-                <ProtectedRoute>
-                  <TablePageContextProvider>
-                    <TablePage />
-                  </TablePageContextProvider>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <AdminPage />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </SocketContextProvider>
-    </StateContextComponent>
+    <SocketContextProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <LoginPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashPageContextProvider>
+                  <Dashboard />
+                </DashPageContextProvider>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/tabela"
+            element={
+              <ProtectedRoute>
+                <TablePageContextProvider>
+                  <TablePage />
+                </TablePageContextProvider>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </SocketContextProvider>
   );
 }
 
