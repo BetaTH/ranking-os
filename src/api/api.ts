39f555474c110d677f4e26a06189d71fbbd5d232
Thielson Almendra <thielson12@gmail.com>
2022-08-11 13:07:38 -0300

    api update

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
-console.log(import.meta.env.VITE_REACT_APP_API_URL);
 
-const url = "https://ranking-os-backend-production.up.railway.app";
+const url = import.meta.env.VITE_REACT_APP_API_URL;
 export const api = axios.create({
   baseURL: url,
 });
-
-//https://ranking-os-backend-production.up.railway.app
